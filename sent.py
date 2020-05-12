8ca2cb2aea7a1c2046a36575d52ae9c421c0c677
William McEachen <wmceachen@berkeley.edu>
2020-05-11 23:36:38 -0700

    tweet stuff

---
+++ sent.py
@@ -0,0 +1,3 @@
+import pandas as pd
+tweets_df = pd.read_csv('tweets.csv')
+print(tweets_df)
--- twitter_accounts.py
+++ twitter_accounts.py
@@ -1,27 +1,11 @@
 import pandas as pd
 import re
 import swifter
-import twitter
 import matplotlib.pyplot as plt
 from twitterscraper import query_tweets_from_user
-# api = twitter.Api(consumer_key=[consumer key],
-#                   consumer_secret=[consumer secret],
-#                   access_token_key=[access token],
-#                   access_token_secret=[access token secret])
-earlier_data = pd.read_json("bp_accounts/2012_14.json")
-later_data = pd.read_json("bp_accounts/2016_18.json")
-df = pd.concat([earlier_data,later_data])
-# print(df.columns.values)
-df = df[['State', 'Body', 'Party', 'Year', 'Name', 'Campaign Twitter',
-                   'Personal Twitter', 'Official Twitter']]
-# print(df.shape)
+df = pd.read_csv('accounts.csv')
 
-df.dropna(how='all', inplace=True, subset=['Campaign Twitter',
-                                                'Personal Twitter', 'Official Twitter'])
-df.groupby("Year").size().plot(kind='bar', title='Account Totals by Year')
-plt.savefig('election_counts.png')
-# plt.bar(pd.value_counts(df['Year']).index, pd.value_counts(df['Year']))
-# tweets = query_tweets_from_user("Palmer4Alabama", limit=10000)
+
 def get_tweets(row):
     col = ''
     for acct in ['Campaign Twitter', 'Personal Twitter', 'Official Twitter']:
@@ -33,5 +17,10 @@ def get_tweets(row):
     # print(account)
     row['tweets'] = query_tweets_from_user(account, limit=10000)
     return row
-new_df = df.iloc[:5].swifter.apply(get_tweets, axis=1)
-print(new_df)
+
+try:
+    tweets_df = pd.read_csv('tweets.csv')
+except:
+    new_df = df.swifter.apply(get_tweets, axis=1)
+    new_df.to_csv('tweets.csv', index=False)
+print(tweets_df.tweets)
