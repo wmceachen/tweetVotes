4f0f40647fe104a9479e016426d0472d0755abcf
wmceachen <wmceachen@berkeley.edu>
2020-05-13 15:04:17 -0700

    more contractions fun

--- sent_cleaning.py
+++ sent_cleaning.py
@@ -3,6 +3,7 @@ from os import listdir
 from nltk.tokenize import TweetTokenizer
 import swifter
 from utils import clean_text
+import contractions as cont
 df = pd.concat([pd.read_csv('sent/Subtask_A/'+fn, sep='\t', header=None) for fn in listdir('sent/Subtask_A') if fn not in ['sms-2013test-A.tsv', 'livejournal-2014test-A.tsv']])
 df.rename(columns = {1:'sent', 2:'tweet'}, inplace=True)
 df = df[['sent','tweet']]
@@ -12,9 +13,6 @@ pd.read_csv('sent/Subtask_A/livejournal-2014test-A.tsv', sep='\t', header=None)]
 df1 = df1.rename(columns = {2:'sent', 3:'tweet'})[['sent','tweet']]
 df = pd.concat([df, df1])
 df.replace({'neutral': 0, 'positive': 1, 'negative':-1}, inplace=True)
-from pycontractions import Contractions
-cont = Contractions(api_key="word2vec-google-news-300")
-print(list(cont.expand_texts(df.tweet, precise=True)))
 # tknzr = TweetTokenizer()
-# df.tweet = df.tweet.swifter.apply(lambda tweet: tknzr.tokenize(clean_text(tweet)))
-# df.to_csv('sent/trinary_tweets.csv', index=False)
+df.tweet = df.tweet.swifter.apply(lambda tweet: cont.fix(clean_text(tweet)))
+df.to_csv('sent/trinary_tweets.csv', index=False)
