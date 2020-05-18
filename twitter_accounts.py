63d86dc3d1648d89b4503780c6bf42c95b07f3c7
William McEachen <wmceachen@berkeley.edu>
2020-05-17 23:40:44 -0700

    who knew the tweets were wrong

--- twitter_accounts.py
+++ twitter_accounts.py
@@ -2,8 +2,8 @@ import pandas as pd
 import re
 import swifter
 import matplotlib.pyplot as plt
-from twitterscraper import query_tweets_from_user
-df = pd.read_csv('accounts.csv')
+# from twitterscraper import query_tweets_from_user
+import os
 
 
 def get_tweets(row):
@@ -12,15 +12,20 @@ def get_tweets(row):
         if not pd.isna(row[acct]):
             col = acct
             break
-    # print(row[col])
     account = re.search("(?<=twitter\.com/).*", row[col]).group()
-    # print(account)
-    row['tweets'] = query_tweets_from_user(account, limit=10000)
-    return row
+    os.system(f"twitterscraper {account} --user -o tweets/{account}.json")
+    return account
 
-try:
-    tweets_df = pd.read_csv('tweets.csv')
-except:
-    new_df = df.swifter.apply(get_tweets, axis=1)
-    new_df.to_csv('tweets.csv', index=False)
-print(tweets_df.tweets)
+# try:
+#     tweets_df = pd.read_csv('tweets.csv')
+# except:
+df = pd.read_csv('accounts.csv')
+df['Account Scraped'] = df.swifter.apply(get_tweets, axis=1)
+tweet_dfs = []
+for account in os.listdir('./tweets'):
+    account_name = account.partition('.json')[0]
+    account_tweets = pd.read_json('./tweets/'+account)
+    account_tweets['name'] = account_name
+    tweet_dfs.append(account_tweets)
+all_tweets_df = pd.merge(df, pd.concat(tweet_dfs), how='right', left_on='Account Scraped', right_on='name')
+all_tweets_df.to_csv('tweets/all_tweets.csv', index=False)
